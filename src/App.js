74c4f68648cf65f1b5a5eb75d9e9aebc1887c379
Abhishek Sharma <keshav.shm@gmail.com>
2021-04-12 12:10:44 +0530

    added more utils

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Map from './Map.js';
 import Table from './Table.js';
 import { sortData } from './util.js';
 import LineGraph from './LineGraph';
-
+import "leaflet/dist/leaflet.css";
 
 function App() {
   
@@ -17,6 +17,9 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796});
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -39,6 +42,7 @@ function App() {
 
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
       });
     };
@@ -49,14 +53,18 @@ function App() {
     const countryCode = event.target.value;
     setCountry(countryCode);
 
-    const url = countryCode ==='worldwide' ? "https://disease.sh/v3/covid-19/all" 
+    const url = countryCode ==='worldwide' 
+    ? "https://disease.sh/v3/covid-19/all" 
     : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
-     fetch(url)
+      fetch(url)
       .then(response => response.json())
       .then(data => {
         setCountry(countryCode);
         setCountryInfo(data);
+
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -80,7 +88,11 @@ function App() {
         <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
       </div>
 
-      <Map />
+      <Map 
+        countries = {mapCountries}
+        center = {mapCenter}
+        zoom = {mapZoom}
+      />
       </div>
       <Card className="app-right">
         <CardContent>
